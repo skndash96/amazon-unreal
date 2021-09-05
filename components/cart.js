dcb6b6406d4bf2043b7ed3af8b6586874f8d0d2b
skndash96 <dashskndash@gmail.com>
2021-09-05 17:43:58 +0530

    buy items button

--- components/cart.js
+++ components/cart.js
@@ -3,13 +3,23 @@ import { useState, useEffect } from "react";
 import { products } from "../fakedata";
 import Products from "./products";
 import styles from "./cart.module.scss";
+import Popup from './popup'
+import { FaPlus } from 'react-icons/fa'
 
 export default function CartPage() {
     const { cart } = useSelector((state) => state);
     const dispatch = useDispatch();
 
     const [data, setData] = useState(null);
+    const [popup, setPopup] = useState(false)
 
+    const bill = data?.reduce(
+        (cost, item, index) =>
+            (cost + item.price) *
+            cart[index]?.count || 0,
+        0
+    )?.toFixed(2) || 0
+    
     useEffect(() => {
         setData(
             cart.slice().map((item) => ({
@@ -19,6 +29,23 @@ export default function CartPage() {
     }, [cart.length]);
 
     return (
+        <>
+        {popup &&
+        <Popup>
+            <button onClick={() => setPopup(false)} className="close">
+                <FaPlus />
+            </button>
+            <h4 className="title">
+                Become a StarGazer
+            </h4>
+            <p className="description">
+                A <span> star </span> for this site is worth more than <span> {parseInt(bill) || 0} bucks </span> and you can get your cart items for free!
+            </p>
+            <button className="cta">
+                <a href="https://github.com/skndash96/amazon-unreal" target="_blank" rel="noreferrer"> Do it! </a>
+            </button>
+        </Popup>}
+        
         <div className={"container " + styles.cart}>
             {!data ? (
                 <div className="loading">...</div>
@@ -45,15 +72,14 @@ export default function CartPage() {
                                 Total Price:
                                 <span>
                                     <span>$</span>
-                                    {(data.reduce(
-                                        (cost, item, index) =>
-                                            (cost + item.price) *
-                                                cart[index]?.count || 0,
-                                        0
-                                    )).toFixed(2)}
+                                    {bill}
                                 </span>
                             </li>
                         </ul>
+                        
+                        <button className={parseInt(bill) ? "" : "disabled"} onClick={() => parseInt(bill) && setPopup(true)}>
+                            Buy it
+                        </button>
                     </div>
 
                     <Products
@@ -65,5 +91,6 @@ export default function CartPage() {
                 </div>
             )}
         </div>
+    </>
     );
 }
---
+++ components/popup.js
@@ -0,0 +1,12 @@
+import styles from './popup.module.scss'
+
+export default function Popup({ children }) {
+    return (
+        
+        <div className={styles.popup}>
+            <div className="pop">
+                { children }
+            </div>
+        </div>
+    )
+}
